91ddacbb30d7d00b2538ff9347c831f0c5f5c9f1
camillapalombi <camilla.palombi14@gmail.com>
2022-04-29 15:47:50 +0200

    Movie - variabili d'istanza

---
+++ index.php
@@ -0,0 +1,28 @@
+<?php
+
+class Movies {
+
+    private $title;
+    private $year;
+    private $genre;
+    private $duration;
+    private $poster;
+
+    private $originalLanguage;
+    private $originalTitle;
+    private $country;
+    private $filmCompany;
+    private $mainActors;
+    private $director;
+    private $rating;
+    private $description;
+
+}
+
+
+
+
+
+
+
+?>
